a04b6f5dac029b41f60f3c721bb435576beda2d4
Uday <suravarapuuday160@gmail.com>
2026-08-16 23:01:15 +0530

    Add solution for valid parentheses problem
    Implement a solution to validate parentheses using a stack.

---
+++ LeetCode/Stacks/20.Valid_Parenthese.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    stack <char> st;
+    bool isValid(string s) {
+        for(char c:s){
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+                else{
+                    if((c == ')' && st.top() == '(')||
+                    (c == ']' && st.top() == '[')||
+                    (c == '}' && st.top() == '{')){
+                        st.pop();
+                    }
+                    else{
+                    return false;}
+                }
+            }
+        }
+        return st.empty();
+    }
+};
