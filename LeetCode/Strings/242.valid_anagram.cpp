110f877f1a0461c1a45d66df51a2ad855a801333
Uday <suravarapuuday160@gmail.com>
2026-08-26 05:30:21 +0530

    Add isAnagram function with character counting logic
    Implement isAnagram function using character frequency counting.

--- LeetCode/Strings/242.valid_anagram.cpp
+++ LeetCode/Strings/242.valid_anagram.cpp
@@ -1,3 +1,6 @@
+/*
+Approach - Character Hashing
+
 #include <unordered_map>
 class Solution {
 public:
@@ -23,3 +26,35 @@ public:
         }
     }
 };
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        // if the lengths of the string is not equal then
+        // it is obviously not an anagram
+
+        int count[26] = {0};
+        // to count the frequency of each character in the strings
+    
+        for(char ch:s){
+          count[ch - 'a']++;
+        }
+        for(char ch:t){
+            count[ch - 'a']--;
+        }
+        for(int i = 0;i < 26;i ++){
+            if(count[i] != 0){
+                return false;
+                // if it is not zero means 
+                // there are no equal number of characters in the string
+                // returning false
+            }
+        }
+        return true;
+        }
+        
+    };
