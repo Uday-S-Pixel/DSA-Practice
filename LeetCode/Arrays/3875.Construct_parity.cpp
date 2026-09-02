d8b3f829943d2a61563415938119ffa0dd70c6bf
Uday <suravarapuuday160@gmail.com>
2026-09-02 18:00:32 +0530

    Add Solution class with uniformArray method

---
+++ LeetCode/Arrays/3875.Construct_parity.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+
+        // An array containing only even or only odd numbers is already uniform
+        // If both even and odd numbers are present, every even number
+        // can be made odd by subtracting an odd number
+
+        return true;
+    }
+};
